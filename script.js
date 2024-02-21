419043decd38df516cc259638571b36ccaa64435
dwjuston <43318129+dwjuston@users.noreply.github.com>
2024-02-21 16:01:56 +0000

    toggle toc

--- script.js
+++ script.js
@@ -116,3 +116,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', loadEpisodeData(0,0));
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleTocBtn = document.getElementById('toggleToc');
+    const toc = document.getElementById('tableOfContents');
+
+    toggleTocBtn.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the link from navigating
+        toc.classList.toggle('hidden'); // Toggle the visibility of the ToC
+    });
+});
